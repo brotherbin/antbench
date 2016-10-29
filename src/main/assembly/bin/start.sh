7cacf0ad81801cb5dec5178f384333344ac24c8f
brotherbin <brotherbin@outlook.com>
2016-10-29 19:58:27 +0800

    modify start.sh

--- src/main/assembly/bin/start.sh
+++ src/main/assembly/bin/start.sh
@@ -14,4 +14,4 @@ ANT_HOME=`pwd`
 cd $CURR_DIR
 JAVA_OPT="-server -Xms2G -Xmx2G -XX:MaxPermSize=64M  -XX:+AggressiveOpts -XX:MaxDirectMemorySize=2G -DANT_HOME=$ANT_HOME -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=1970 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 CLASS_PATH="$ANT_HOME/conf:$ANT_HOME/lib/*"
-$JAVA_CMD $JAVA_OPT -cp $CLASS_PATH $MAIN_CLASS $MAIN_ARGS $MAIN_ARGS
+$JAVA_CMD $JAVA_OPT -cp $CLASS_PATH $MAIN_CLASS $MAIN_ARGS
